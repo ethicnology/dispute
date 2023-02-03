9d47a0c49750ce46407f7138a3466e776470143c
ethicnology <ethicnology@pm.me>
2023-02-03 15:50:49 +0200

    refactor: dart-nostr 1.3.2

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,9 @@ import 'package:dispute/model/profile.dart';
 import 'package:dispute/screen/home.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
-import 'package:nostr/nostr.dart';
 import 'package:provider/provider.dart';
 
 final logger = Logger();
-
-String generateSubscription() {
-  return '["REQ","${generate64RandomHexChars()}",{"kinds":[1],"since":${currentUnixTimestampSeconds() - 86400}}]';
-}
 
 void main() {
   runApp(
