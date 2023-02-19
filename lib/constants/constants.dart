6a2813d8c668e71da4edb1d64d56e0b72106c1f9
A.Rahman Mufarrah <a.mufarrah@gmail.com>
2023-02-19 19:03:33 +0300

    feat: add unique avatar for users based on pubkey and added errorBuilder for network image

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const buttonBackgroundColor = Color(0XFF32246B);
 const buttonBorderColor = Color(0XFFB2A5E3);
+String avatarUrl = "https://robohash.org/";
+String imagesize = "size=200x200";
