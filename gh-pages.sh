a601f8598ab18ecc57b6e6a960e802da0241daea
ethicnology <ethicnology@pm.me>
2023-05-21 10:41:40 +0200

    fix: gh-pages deployment

--- gh-pages.sh
+++ gh-pages.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ethicnology/dispute.git main:gh-pages #main or master
+git push -f git@github.com:ethicnology/dispute.git master:gh-pages #main or master
 
 cd -
